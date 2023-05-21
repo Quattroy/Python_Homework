177c7e6c5c06d4c78f3e99983c8ad9eced6061a4
Igor Isupov <igor-i.s1@ya.ru>
2023-05-21 22:41:50 +0300

    Задача 2

--- Seminar1/task_2.py
+++ Seminar1/task_2.py
@@ -2,3 +2,10 @@
 # *Пример:*
 # 123 -> 6 (1 + 2 + 3)
 # 100 -> 1 (1 + 0 + 0) |
+
+num = int(input("Введите трехзначное число: "))
+num1 = num % 10
+num2 = num // 10 % 10
+num3 = num // 100
+result = num1 + num2 + num3
+print(f"Суммы чисел: {num3} + {num2} + {num1}  =  {result}")
