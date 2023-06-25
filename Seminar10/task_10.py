d0414b5f71d6a95f37602e58c933c44c16c40071
Igor Isupov <igor-i.s1@ya.ru>
2023-06-25 22:07:45 +0300

    Задание №10

---
+++ Seminar10/task_10.py
@@ -0,0 +1,22 @@
+
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+import random
+lst = ['robot'] * 10
+lst += ['human'] * 10
+random.shuffle(lst)
+data = pd.DataFrame({'whoAmI':lst})
+print(data.head())
+
+
+
+data['tmp'] = 1
+data.set_index([data.index, 'whoAmI'], inplace=True)
+data = data.unstack(level=-1, fill_value = 0).astype(int)
+data.columns = data.columns.droplevel()
+data.columns.name = None
+print(data)
